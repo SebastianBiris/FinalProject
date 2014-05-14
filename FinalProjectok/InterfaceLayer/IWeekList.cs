20103e30ba6080f4360d1adea9af30e6bf14359d
SebastianBiris <sebgarcea@yahoo.co.uk>
2014-05-14 11:24:20 +0200

    IWeekList
    done

---
+++ FinalProjectok/InterfaceLayer/IWeekList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ModelLayer
+{
+   public interface IWeekList
+    {
+        DateTime Day1 { get; set; }
+        DateTime Day2 { get; set; }
+        DateTime Day3 { get; set; }
+        DateTime Day4 { get; set; }
+        DateTime Day5 { get; set; }
+        DateTime Day6 { get; set; }
+        DateTime Day7 { get; set; }
+        DateTime GetDay(string day);
+        int Year { get; }
+    }
+}
